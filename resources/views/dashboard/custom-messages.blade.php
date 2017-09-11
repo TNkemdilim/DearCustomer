17f0ea09de8681abd526d20bd8f67c88d9b983e8
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2017-09-11 13:38:41 +0100

    Redesign custom message view

--- resources/views/dashboard/custom-messages.blade.php
+++ resources/views/dashboard/custom-messages.blade.php
@@ -12,20 +12,24 @@
     @endphp
 
     @foreach($custom_messages as $custom_message)
-
+    <!-- Column -->
+    
     <div class="col-lg-3">
         <div class="card">
             <img class="card-img-top img-responsive" src="{{URL::asset('images/big/img1.jpg')}}" alt="Card">
             <div class="card-block">
                 <ul class="list-inline font-14">
-                    <li class="p-l-0">20 May 2016</li>
-                    <li><a href="javascript:void(0)" class="link">5 people</a></li>
+                    <li class="p-l-0">{{ $custom_message->updated_at->format('dS M, Y') }}</li>
+                    @php
+                      $logs_count = App\SingleLog::where('custom_message_id', $custom_message->id)->count();
+                    @endphp
+                    <li class="pull-right"><a href="javascript:void(0)" class="link">{{ $logs_count }}  <i class="fa fa-user"></i></a></li>
                 </ul>
-                <h3 class="font-normal">Featured Hydroflora Pots Garden &amp; Outdoors</h3>
+                <h3 class="font-normal"><a href="javascript:void(0)" class="link"> {{ $custom_message->title }} </a></h3>
             </div>
         </div>
     </div>
-    <!-- Column -->
+    <!-- Column -->{{-- &amp; 20 May 2016 --}}
 
     @endforeach
 </div>
